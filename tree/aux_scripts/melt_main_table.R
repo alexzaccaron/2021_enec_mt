48725c51bdbcf9138e0920696396fec26b259e52
ggg298 account 07 <ggg298-07@farm.cse.ucdavis.edu>
2021-03-01 19:41:53 -0800

    added new version of tree folder with MrBayes tree

--- tree/aux_scripts/melt_main_table.R
+++ tree/aux_scripts/melt_main_table.R
@@ -6,10 +6,10 @@ main_table_fname        = args[1]
 main_table_melted_fname = args[2]
 
 # reshape package to call melt() functiom
-if (!require("reshape")) install.packages("reshape")
+if (!require("reshape")) install.packages("reshape", repos = "http://cran.us.r-project.org")
 
 # reading main table with species info and accession numbers
-main_table = read.table(main_table_fname, sep = '\t', header = T, fill = T)
+main_table = read.csv(main_table_fname, header = T, fill = T)
 
 # I will select these fields
 fields = c("atp6", "nad1","nad2","nad3","nad4","nad4L","nad5","nad6","cox1","cox2","cox3","cob","TaxID","Organism")
